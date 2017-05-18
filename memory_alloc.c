226f53045387339634259a6599e80b3f6e136096
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-05-18 18:52:58 +0000

    memory exercises

---
+++ memory_alloc.c
@@ -0,0 +1,44 @@
+// This chapter explains dynamic memory management in C. 
+// The C programming language provides several functions for memory
+// allocation and management. These functions can be found in the <stdlib.h> header
+// file.
+
+/*
+void *calloc(int num, int size); //This function allocates an array of # elements each of size in bytes will be size.
+
+void free(void *address); //This function releases a block of memory block specified by address.
+
+void *malloc(int num); //This function allocates an array of num bytes and leave them uninitialized.
+
+void *realloc(void *address, int newsize); //This function re-allocates memory extending it upto newsize.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+
+int main(  ) {
+
+  char name[100];
+  char *description;
+
+  strcpy(name, "Zara Ali");
+
+  /* allocate memory dynamically */
+
+  description = malloc( 200 * sizeof(char) );
+
+  if ( description == NULL ) {
+    fprintf(stderr, "Error - unable to allocate required memory\n");
+  }
+
+  else {
+    strcpy( description, "Zara ali a DPS student in class 10th");
+  }
+
+  printf("Name = %s\n", name);
+
+  printf("Description: %s\n", description );
+
+}
