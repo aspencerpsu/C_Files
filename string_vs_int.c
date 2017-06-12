5b86dded87f75079bea38a63984ce140cd8b67a4
Akeem Leighton Foster Spencer <aspencerpsu@gmail.com>
2017-06-11 21:17:11 -0400

    string vs int

---
+++ string_vs_int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(  ) {
+  int chan;
+  chan = 1;
+  chan = chan - '0';
+  printf("Channel value is %d\n", chan);
+  return 0;
+}
